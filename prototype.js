803ee1f682e57ae65005749b860e39d268c0f2a7
maneeratkn <maneerat.m@entronica.co.th>
2019-05-08 15:34:13 +0700

    Create project and test mobile type

---
+++ prototype.js
@@ -0,0 +1,81 @@
+
+// function test() {
+//     var username = document.getElementById("username").value;
+//     var password = document.getElementById("password").value;
+
+//     // if (confirm("Loading .. ")) {
+//         if ( username == "01" && password == "01"){
+//             // window.location.href="http://localhost:4200/loading"
+//             console.log(username);
+//             console.log(password);
+//             return false;
+      
+//           }else if( username == "02" && password == "02"){
+//             window.location.href="http://localhost:4200/success";
+//             console.log("success");
+//             return false;
+      
+//           }else{
+           
+//             alert('ERROR !');
+//             window.location.href="http://localhost:4200/error"
+//             console.log("error");
+//             return false;
+          
+//           }
+//     // } else {
+//     //     alert('bye');
+//     // }
+
+//     document.getElementById("id01").innerHTML = txt;
+// }
+    function test(){
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+       
+        if("01"==username){
+            console.log(username);
+            alert(username + " : " + password);
+            //  window.location.href="customer.html";
+            // return flase;
+        //    แนนลองเปิดบรรทัดที่เป็นเปิดหน้าใหม่ดูนะพี่ว่าน่าจะได้แล้วละ พี่ดัก 01 ไว้อย่าลืมน้า
+        }
+    }
+
+    
+
+// function test() {
+// console.log("fff");
+//         if (myform.username.value=="01") { 
+//             if (myform.password.value=="01") {              
+//                 location="customer.html" 
+//             } else {
+//                 alert("Invalid Password")
+//             }
+//         } else {  
+//             alert("Invalid UserID")
+//             }
+//         }
+//     var username = document.getElementById("username").value;
+//     var password = document.getElementById("password").value;
+//    console.log(username);
+//     if ( username == "01" && password == "01"){
+     
+//       window.location.href="http://localhost:4200/customer";
+//       console.log("loading");
+//       return false;
+
+//     }else if( username == "02" && password == "02"){
+//       window.location.href="http://localhost:4200/success";
+//       console.log("success");
+//       return false;
+
+//     }else{
+     
+//       alert('ERROR !');
+//       window.location.href="http://localhost:4200/error"
+//       console.log("error");
+//       return false;
+    
+//     }
+// }
